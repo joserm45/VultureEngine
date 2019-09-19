87ae817f8e45675094e411d939179e448b4ed97c
joserm45 <joserodriguez@escolatecnos.cat>
2019-09-19 12:12:59 +0200

    Preparing Engine

---
+++ VultureEngine/ModuleSceneIntro.cpp
@@ -0,0 +1,49 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->physics->debug = true;
+	//Music Level
+	//PlaySceneMusic();
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+
+	return UPDATE_CONTINUE;
+}
+
+
+
+
+
+void ModuleSceneIntro::PlaySceneMusic()
+{
+	App->audio->PlayMusic("Sound/Music.ogg", 0);
+}
---
+++ VultureEngine/ModuleSceneIntro.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Module.h"
+#include "p2DynArray.h"
+#include "Globals.h"
+#include "Primitive.h"
+
+
+struct PhysBody3D;
+struct PhysMotor3D;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+
+	void PlaySceneMusic();
+
+
+};
