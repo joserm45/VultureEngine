f4a522736cd6174ec3fe66fa5e61fbb76af9a166
joserm45 <joserodriguez@escolatecnos.cat>
2019-09-24 20:46:04 +0200

    Restructuring all

---
+++ Source/ModuleSceneIntro.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Module.h"
+#include "p2DynArray.h"
+#include "Globals.h"
+#include "Primitive.h"
+
+
+struct PhysBody3D;
+struct PhysMotor3D;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+
+	void PlaySceneMusic();
+
+	bool my_tool_active = true;
+	
+	char buf[10] = "casa";
+	float f = 1.0f;
+	//float my_color[] = { 0.0f, 0.0f, 1.0f };
+};
